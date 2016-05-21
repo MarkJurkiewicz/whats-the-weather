b68a16de5c8bd441f4627b33b644f85ed5efd3b3
Mark Jurkiewicz <markajurkiewicz@gmail.com>
2016-05-21 01:21:08 -0700

    successful ajax call to openweather API able read temp

---
+++ weather.js
@@ -0,0 +1,42 @@
+/**
+ * Created by maj on 5/20/16.
+ */
+
+
+
+
+$(function () {  //doc ready
+
+    if ("geolocation" in navigator) {
+    $('#showTemp').click(function () {
+        navigator.geolocation.getCurrentPosition(gotLocation);
+    });
+    } else {
+        alert('Your browser doesnt support geolocation. Sorry.');
+    }
+
+});
+
+
+
+function gotLocation(position) {
+    var lat = position.coords.latitude; // finds latitude
+    var lon = position.coords.longitude; // finds longitude
+    var api_key = '302a96b6941cd7ba4c9a75d0333859b2';
+    var api_url = 'http://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + lon + '&units=metric&appid=' + api_key;
+
+
+    console.log(position);
+
+    //ajax call to Openweather.com API
+    $.ajax({
+        dataType: 'json',
+        url : api_url,
+        method : 'GET',
+        success : function (data) {
+            var allData = data.main.temp;
+            $('#result').text(allData + '°');
+            console.log(data);
+        }
+    });
+}
